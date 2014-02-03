e9d31b1c2737ec52b2ebc51161190e18c6afc1f8
Michael Pope <amorphid@captaincoder.io>
2014-02-02 17:45:35 -0800

    Create test for SampleDelayedJob#description
    [ticket: 64978296]

--- spec/models/sample_delayed_job_spec.rb
+++ spec/models/sample_delayed_job_spec.rb
@@ -1,4 +1,13 @@
 require "spec_helper"
 
 describe SampleDelayedJob do
+  subject { SampleDelayedJob.create }
+
+  context "#description" do
+    it "diplays message, includng record ID" do
+      description = subject.description
+      subject_id  = subject.id.to_s
+      expect(description).to eq("SampleDelayedJob # "+ subject_id)
+    end
+  end
 end
