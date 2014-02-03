bcfe3b93aed9d78e7b5f7785ea86ac469ff882fa
Michael Pope <amorphid@captaincoder.io>
2014-02-02 20:06:52 -0800

    SampleDelayedJobsController#index sets @sample_delayed_jobs
    [ticket: 64980842]

--- spec/controllers/sample_delayed_jobs_controller_spec.rb
+++ spec/controllers/sample_delayed_jobs_controller_spec.rb
@@ -7,5 +7,12 @@ describe SampleDelayedJobsController do
       response_code = response.response_code
       expect(response_code).to eq(200)
     end
+
+    it "sets @sample_delayed_jobs" do
+      sample_delayed_jobs = Fabricate.times(2, :sample_delayed_job)
+      get :index
+      assignment = assigns(:sample_delayed_jobs)
+      expect(assignment).to match_array(sample_delayed_jobs)
+    end
   end
 end
