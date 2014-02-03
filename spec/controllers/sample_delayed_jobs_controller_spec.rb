b7fcdd8d3b1bbbbba1d3af5e881c3379faa84077
Michael Pope <amorphid@captaincoder.io>
2014-02-02 18:48:22 -0800

    Create test for SampleDelayedJobsController#index returns HTTP status code 200
    [ticket: 64979174]

--- spec/controllers/sample_delayed_jobs_controller_spec.rb
+++ spec/controllers/sample_delayed_jobs_controller_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helper"
 
 describe SampleDelayedJobsController do
+  context "#index" do
+    it "returns HTTP status code 200" do
+      get :index
+      expect(response).to eq(200)
+    end
+  end
 end
